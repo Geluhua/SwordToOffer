9c70eec99e922faa26243674974a24b68379092a
wangzhengyi <wzyll1314@gmail.com>
2016-03-03 00:01:44 +0800

    面试题4:替换空格

---
+++ src/main/java/com/wzy/swordoffer/ReplaceBlank/ReplaceBlank.java
@@ -0,0 +1,53 @@
+package com.wzy.swordoffer.ReplaceBlank;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * 题目:请实现一个函数，把字符串中的每个空格替换成"%20"
+ * 九度OJ链接：http://ac.jobdu.com/problem.php?pid=1510
+ */
+public class ReplaceBlank {
+    private static String replaceBlank(String input) {
+        if (input == null || input.length() == 0) {
+            return null;
+        }
+        char[] srcInputs = input.toCharArray();
+        int numBlanks = 0;
+        for (int i = 0; i < srcInputs.length; i ++) {
+            if (srcInputs[i] == ' ') {
+                numBlanks ++;
+            }
+        }
+
+        char[] destOutputs = new char[srcInputs.length + 2 * numBlanks];
+        int i = srcInputs.length - 1, j = destOutputs.length - 1;
+        while (i >= 0) {
+            if (srcInputs[i] != ' ') {
+                destOutputs[j --] = srcInputs[i];
+            } else {
+                destOutputs[j --] = '0';
+                destOutputs[j --] = '2';
+                destOutputs[j --] = '%';
+            }
+            i --;
+        }
+
+        return new String(destOutputs);
+    }
+
+    private static String replaceBlank1(String input) {
+        return input.replaceAll(" ", "%20");
+    }
+
+    public static void main(String[] args) throws IOException{
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            String inputStr = scanner.nextLine();
+            System.out.println(replaceBlank1(inputStr));
+        }
+
+        scanner.close();
+    }
+}
