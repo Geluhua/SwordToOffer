d3084f92c9cbd3739e73f6ce9b08f693c43e7249
wangzhengyi <wzyll1314@gmail.com>
2016-03-21 09:12:10 +0800

    面试题12：打印1到最大N位数

---
+++ src/main/java/com/wzy/swordoffer/Print1ToMaxOfNDigits/Print1ToMaxOfNDigits.java
@@ -0,0 +1,69 @@
+package com.wzy.swordoffer.Print1ToMaxOfNDigits;
+
+import java.util.Scanner;
+
+/**
+ * 题目12:打印1到最大的n位数
+ */
+public class Print1ToMaxOfNDigits {
+    private static void print1ToMaxN(int n) {
+        if (n <= 0) {
+            System.out.println(0);
+            return;
+        }
+
+        char[] nums = new char[n + 1];
+        for(int i = 0; i <= n; i ++) {
+            nums[i] = '0';
+        }
+
+        while (!increment(nums, n)) {
+            printNums(nums, n);
+        }
+    }
+
+    private static void printNums(char[] nums, int n) {
+        int ed = n - 1;
+        for (; ed >= 0; ed ++) {
+            if (nums[ed] != '0') {
+                break;
+            }
+        }
+
+        for (int i = ed; i >= 0; i --) {
+            System.out.printf("%c", nums[i]);
+        }
+        System.out.println();
+    }
+
+    private static boolean increment(char[] nums, int n) {
+        if (nums[n] != '0') {
+            return true;
+        }
+
+        int inc = 0;
+        for (int i = 0; i <= n; i ++) {
+            int num = nums[i] - '0';
+            if (num + inc + 1 <= 9) {
+                nums[i] = (char) (nums[i] + inc + 1);
+                break;
+            } else {
+                int yn = (num + inc + 1) % 10;
+                inc = (num + inc + 1) / 10;
+                nums[i] = (char) ('0' + yn);
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        while (cin.hasNext()) {
+            int n = cin.nextInt();
+            print1ToMaxN(n);
+        }
+
+        cin.close();
+    }
+}
