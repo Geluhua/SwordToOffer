fdcef79ccff750d5780ccb8ffb01bba46886cc8e
wangzhengyi <wzyll1314@gmail.com>
2016-03-29 17:58:46 +0800

    提交lru算法

---
+++ src/main/java/com/wzy/swordoffer/Cache/LRUCache.java
@@ -0,0 +1,120 @@
+package com.wzy.swordoffer.Cache;
+
+import java.util.HashMap;
+
+/**
+ * LRU缓存算法实现.
+ */
+@SuppressWarnings("unused")
+public class LRUCache {
+    private final int mCapacity;
+    private int mCurrentNum;
+    private HashMap<Integer, DoubleLinkedList<Integer>> mHashMap;
+    private DoubleLinkedList<Integer> mHeader;
+
+    public LRUCache(int capacity) {
+        mCapacity = capacity;
+        mCurrentNum = 0;
+        mHeader = new DoubleLinkedList<>();
+        mHashMap = new HashMap<>();
+    }
+
+    public int get(int key) {
+        if (mHashMap.containsKey(key)) {
+            DoubleLinkedList<Integer> node = mHashMap.get(key);
+            // Move node to head
+            if (node.prev == mHeader) {
+                // do nothing, already the first node
+            } else if (node.next == mHeader) {
+                // is the last node
+                node.prev.next = mHeader;
+                mHeader.prev = node.prev;
+                mHeader.next.prev = node;
+                node.next = mHeader.next;
+                node.prev = mHeader;
+                mHeader.next = node;
+            } else {
+                // is the middle node
+                node.prev.next = node.next;
+                node.next.prev = node.prev;
+                mHeader.next.prev = node;
+                node.next = mHeader.next;
+                node.prev = mHeader;
+                mHeader.next = node;
+            }
+            return node.value;
+        } else {
+            return -1;
+        }
+    }
+
+    public void set(int key, int value) {
+        DoubleLinkedList<Integer> node = null;
+        if (mHashMap.containsKey(key)) {
+            node = mHashMap.get(value);
+        }
+
+        if (node == null) {
+            // new node
+            node = new DoubleLinkedList<>();
+            node.value = value;
+            mCurrentNum ++;
+            if (mCurrentNum > mCapacity) {
+                // delete node from end
+                if (mHeader.prev != null && mHeader.prev != mHeader) {
+                    mHashMap.remove(mHeader.prev.key);
+                    mHeader.prev.prev.next = mHeader;
+                    mHeader.prev = mHeader.prev.prev;
+                }
+            }
+            // insert new node
+            mHeader.next.prev = node;
+            node.next = mHeader.next;
+            node.prev = mHeader;
+            mHeader.next = node;
+            node.key = key;
+        } else {
+            // exist node, move node to head
+            if (node.prev == mHeader) {
+                // do nothing, already the first node
+            } else if (node.next == mHeader) {
+                // is the last node
+                node.prev.next = mHeader;
+                mHeader.prev = node.prev;
+                mHeader.next.prev = node;
+                node.next = mHeader.next;
+                node.prev = mHeader;
+                mHeader.next = node;
+            } else {
+                // is the middle node
+                node.prev.next = node.next;
+                node.next.prev = node.prev;
+                mHeader.next.prev = node;
+                node.next = mHeader.next;
+                node.prev = mHeader;
+                mHeader.next = node;
+            }
+        }
+        mHashMap.put(key, node);
+    }
+
+    private static class DoubleLinkedList<T> {
+        DoubleLinkedList<T> prev;
+        DoubleLinkedList<T> next;
+        int key;
+        T value;
+        public DoubleLinkedList() {
+            prev = next = this;
+        }
+    }
+
+    public static void main(String[] args) {
+        LRUCache lruCache = new LRUCache(1);
+        lruCache.set(2, 1);
+        int value = lruCache.get(2);
+        lruCache.set(3, 2);
+        value = lruCache.get(2);
+        value = lruCache.get(3);
+        System.out.println(value);
+    }
+}
