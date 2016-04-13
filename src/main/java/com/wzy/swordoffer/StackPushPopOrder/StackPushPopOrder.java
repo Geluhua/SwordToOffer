ef401475c3768a4250db0aaefb9d2fafa3982e26
wangzhengyi <wzyll1314@gmail.com>
2016-04-13 09:14:29 +0800

    完成面试题22:栈的压入,弹出序列

--- src/main/java/com/wzy/swordoffer/StackPushPopOrder/StackPushPopOrder.java
+++ src/main/java/com/wzy/swordoffer/StackPushPopOrder/StackPushPopOrder.java
@@ -11,19 +11,35 @@ public class StackPushPopOrder {
     public static boolean isPopOrder(int n, int[] pushList, int[] popList) {
         if (pushList == null || popList == null) return false;
 
-        boolean res = true;
+        int index = 0;
         Deque<Integer> stack = new ArrayDeque<Integer>(n);
         for (int i = 0; i < n; i ++) {
-            int popNum = popList[i];
-            if (stack.isEmpty()) {
-                // 如果栈为空,则入栈
-
+            if (!stack.isEmpty() && stack.peek() == popList[i]) {
+                stack.pop();
+            } else {
+                index = pushStack(popList[i], index, pushList, stack);
+                if (index >= pushList.length) {
+                    return false;
+                }
+                index ++;
+                stack.pop();
             }
-
-
         }
 
-        return res;
+        return true;
+    }
+
+    private static int pushStack(int target, int index, int[] pushList, Deque<Integer> stack) {
+        if (index >= pushList.length) return index;
+
+        for (int len = pushList.length; index < len; index ++) {
+            stack.push(pushList[index]);
+            if (pushList[index] == target) {
+                break;
+            }
+        }
+
+        return index;
     }
 
     public static void main(String[] args) {
