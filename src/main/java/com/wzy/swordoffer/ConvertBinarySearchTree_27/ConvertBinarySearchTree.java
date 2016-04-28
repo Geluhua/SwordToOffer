c363789ca71be3fdbbd57c7e9c2a5f8ffcfab3d8
wangzhengyi <wzyll1314@gmail.com>
2016-04-28 19:01:31 +0800

    提交代码框架

--- src/main/java/com/wzy/swordoffer/ConvertBinarySearchTree_27/ConvertBinarySearchTree.java
+++ src/main/java/com/wzy/swordoffer/ConvertBinarySearchTree_27/ConvertBinarySearchTree.java
@@ -1,5 +1,7 @@
 package com.wzy.swordoffer.ConvertBinarySearchTree_27;
 
+import java.util.Scanner;
+
 /**
  * 面试题27:二叉搜索树与双向链表.
  */
@@ -13,5 +15,31 @@ public class ConvertBinarySearchTree {
 
     public static void convert(TreeNode root) {
 
+    }
+
+    public static TreeNode createBTree(int[] preOrder, int i) {
+        if (preOrder == null || preOrder.length == 0 || i >= preOrder.length || preOrder[i] == 0) {
+            return null;
+        }
+
+        TreeNode root = new TreeNode();
+        root.val = preOrder[i];
+
+        root.left = createBTree(preOrder, i + 1);
+        root.right = createBTree(preOrder, i + 2);
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+
+        while (cin.hasNext()) {
+            int n = cin.nextInt();
+
+
+        }
+
+        cin.close();
     }
 }
