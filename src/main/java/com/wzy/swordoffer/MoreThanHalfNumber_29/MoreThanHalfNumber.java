4c923013c37655221f4f0b7c26d31e1ec7c0e1d0
wangzhengyi <wzyll1314@gmail.com>
2016-06-01 12:20:38 +0800

    面试题29:提供两种AC解法解决数组中超过一半的数字

--- src/main/java/com/wzy/swordoffer/MoreThanHalfNumber_29/MoreThanHalfNumber.java
+++ src/main/java/com/wzy/swordoffer/MoreThanHalfNumber_29/MoreThanHalfNumber.java
@@ -1,5 +1,6 @@
 package com.wzy.swordoffer.MoreThanHalfNumber_29;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -8,19 +9,22 @@ import java.util.Scanner;
  * 面试题29: 数组中出现次数超过一半的数字
  */
 public class MoreThanHalfNumber {
-    private static int findMoreThanHalfNumber(int[] arr, int n) {
-        Map<Integer, Integer> map = new HashMap<>(n);
+    /**
+     * 使用HashMap,时间复杂度O(N),空间复杂度O(N).
+     */
+    private static int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
 
-        for (int e : arr) {
-            if (map.containsKey(e)) {
-                map.put(e, map.get(e) + 1);
+        for (int i = 0, n = nums.length; i < n; i ++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
             } else {
-                map.put(e, 1);
+                map.put(nums[i], 1);
             }
         }
 
         for (Map.Entry<Integer,Integer> entry : map.entrySet()) {
-            if (entry.getValue() > n / 2) {
+            if (entry.getValue() > nums.length / 2) {
                 return entry.getKey();
             }
         }
@@ -28,18 +32,42 @@ public class MoreThanHalfNumber {
         return -1;
     }
 
+    /**
+     * 分析数组结构,时间复杂度O(N),空间复杂度O(1).
+     */
+    public static int majorityElement2(int[] nums) {
+        int res = nums[0], occur = 0;
+
+        for (int i = 1, n = nums.length; i < n; i ++) {
+            if (occur == -1) {
+                res = nums[i];
+                occur = 0;
+                continue;
+            }
+
+            if (nums[i] == res) {
+                occur ++;
+            } else {
+                occur --;
+            }
+        }
+
+        return res;
+    }
+
+
     public static void main(String[] args) {
         int n;
-        int[] arr = new int[100001];
         Scanner cin = new Scanner(System.in);
 
         while (cin.hasNext()) {
             n = cin.nextInt();
+            int[] nums = new int[n];
             for (int i = 0; i < n; i ++) {
-                arr[i] = cin.nextInt();
+                nums[i] = cin.nextInt();
             }
 
-            int element = findMoreThanHalfNumber(arr, n);
+            int element = majorityElement2(nums);
 
             System.out.println(element);
         }
